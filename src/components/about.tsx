8686655ffaa0303f969aeb401225da4635265f35
humbertoham <hamduranhumberto@gmail.com>
2023-12-20 10:41:36 -0600

    first commit

---
+++ src/components/about.tsx
@@ -0,0 +1,23 @@
+
+export default function About() {
+    return (
+      <>
+        
+        <section className="bg-white " id="acerca">
+    <div className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
+        <div className="font-light text-gray-500 sm:text-lg ">
+            <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 ">¿Quiénes somos?</h2>
+            <p className="mb-4">Somos una agencia de profesionales dedicados apasionadamente al mundo inmobiliario. Nos especializamos en facilitar procesos de compra, venta y alquiler de propiedades, brindando un servicio integral que va más allá de las transacciones convencionales. Nuestro compromiso es proporcionar a nuestros clientes una experiencia inigualable en el mercado inmobiliario, respaldada por un equipo de expertos altamente capacitados.</p>
+        </div>
+        <div className="grid grid-cols-2 gap-4 mt-8">
+            <img className="w-full rounded-lg" src="./images/about1.jpg" alt="office content 1"/>
+            <img className="mt-4 w-full lg:mt-10 rounded-lg" src="./images/about2.jpg" alt="office content 2"/>
+        </div>
+    </div>
+    
+</section>
+      s
+      </>
+    )
+  }
+  
---
+++ src/components/banner.tsx
@@ -0,0 +1,25 @@
+
+
+export default function Banner() {
+
+  return (
+    <div className=' mx-auto '>
+    <div className=' relative'>
+      {/* Overlay */}
+      <div className='absolute w-full h-full text-gray-200  bg-black/40 flex flex-col justify-center'>
+        <h1 className='px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold'>
+          Las <span className='text-amber-200'>mejores</span>
+        </h1>
+        <h1 className='px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold'>
+          <span className='text-amber-200'>casas</span> del mercado.
+        </h1>
+      </div>
+      <img
+        className='w-full max-h-[800px] object-cover'
+        src='./images/banner.jpg'
+        alt='/'
+      />
+    </div>
+  </div>
+  )
+}
